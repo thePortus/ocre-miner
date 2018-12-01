b68b6bff2ccff4a8acb0ce9859d020d38ff5c0a0
David Thomas <dave.a.base@gmail.com>
2018-12-01 17:19:17 -0500

    initial phase completed, search and results work to get working json result

--- ocre/record.py
+++ ocre/record.py
@@ -3,7 +3,7 @@ import json
 from collections import UserList
 
 from .base import BaseRequest
-from .settings import ROOT_URL, JSON_API_URL
+from .settings import ROOT_URL, JSON_API_URL, NOMISMA_URL
 
 
 class JSONRecord(BaseRequest):
@@ -35,25 +35,199 @@ class BaseRecords(UserList):
         for record_id in record_ids:
             self.data.append(self.record_object(record_id, options))
 
+    @classmethod
+    def convert(self, parent_data, keyword=None):
+        """Helper function to get data from inside data returned by .get().
+        If parent data is a string, simply removes the url prefix before
+        pertinent information, if a list of dicts, will build a list of
+        data inside each dict found at a given keyword."""
+        retreived_values = []
+        new_value = None
+        # build url to trim from the 'id' entry of coins
+        url_trim = ROOT_URL + JSON_API_URL
+        if type(parent_data) == str:
+            new_value = parent_data.replace(url_trim, '')
+            return new_value
+        if type(parent_data) != list:
+            raise Exception('Convert data must contain list or string.')
+        # otherwise if a list of strings, start looping and getting values
+        for internal_dict in parent_data:
+            # if a string, simply trim nomisma's url prefix
+            if type(internal_dict) == str:
+                new_value = internal_dict.replace(NOMISMA_URL, '')
+                retreived_values.append(new_value)
+            elif keyword in internal_dict:
+                new_value = internal_dict[keyword]
+                # for urls, remove the nomisma site url prefix
+                if type(new_value) == str:
+                    new_value = new_value.replace(NOMISMA_URL, '')
+                retreived_values.append(new_value)
+        # if only one value found, don't return as list, return value itself
+        if len(retreived_values) == 1:
+            return retreived_values[0]
+        return retreived_values
+
     def get(self):
         """Gets all records as dicts in parent dict with ids as keywords."""
-        completed_data = {}
+        raw_data = {}
+        converted_data = {}
+        new_data = None
         counter = 0
         total_records = len(self.data)
         for record in self.data:
             if not self.options['silent']:
                 print(
-                    'Record {}/{} ({}%)'
+                    ' Record {}/{} ({}%): '
                     .format(
                         counter + 1,
                         total_records,
                         round(((counter + 1) / total_records) * 100, 2)
                     ),
-                    end=' '
+                    end=''
                 )
-            completed_data[record.record_id] = record.get()
+            raw_data[record.record_id] = record.get()
             counter += 1
-        return completed_data
+            # go to dictionary inside json-ld containing the actual save_data
+            new_data = raw_data[record.record_id]['@graph']
+            # convert from JSON-ld to plain JSON to make easier to use
+            converted_data[record.record_id] = {}
+            try:
+                converted_data[record.record_id][
+                    'id'
+                ] = self.convert(
+                    new_data[0]['@id']
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'label'
+                ] = self.convert(
+                    new_data[0]['skos:prefLabel']
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'source'
+                ] = self.convert(
+                    new_data[0]['dcterms:source'], '@id'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'manufacture'
+                ] = self.convert(
+                    new_data[0]['nmo:hasManufacture'], '@id'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'denomination'
+                ] = self.convert(
+                    new_data[0]['nmo:hasDenomination'], '@id'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'material'
+                ] = self.convert(
+                    new_data[0]['nmo:hasMaterial'], '@id'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'authority'
+                ] = self.convert(
+                    new_data[0]['nmo:hasAuthority'], '@id'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'mint'
+                ] = self.convert(
+                    new_data[0]['nmo:hasMint'], '@id'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'region'
+                ] = self.convert(
+                    new_data[0]['nmo:hasRegion'], '@id'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'start_date'
+                ] = int(self.convert(
+                    new_data[0]['nmo:hasStartDate'], '@value'
+                ))
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'end_date'
+                ] = int(self.convert(
+                    new_data[0]['nmo:hasEndDate'], '@value'
+                ))
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'obverse_legend'
+                ] = self.convert(
+                    new_data[1]['nmo:hasLegend'], '@value'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'obverse_description'
+                ] = self.convert(
+                    new_data[1]['dcterms:description'], '@value'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'obverse_portraits'
+                ] = self.convert(
+                    new_data[1]['nmo:hasPortrait'], '@id'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'reverse_legend'
+                ] = self.convert(
+                    new_data[1]['nmo:hasLegend'], '@value'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'reverse_description'
+                ] = self.convert(
+                    new_data[1]['dcterms:description'], '@value'
+                )
+            except:
+                pass
+            try:
+                converted_data[record.record_id][
+                    'reverse_portraits'
+                ] = self.convert(
+                    new_data[1]['nmo:hasPortrait'], '@id'
+                )
+            except:
+                pass
+        return converted_data
 
     def save(self, filepath, overwrite=False):
         """Performs common saving pre-operations for child classes."""
@@ -67,7 +241,7 @@ class BaseRecords(UserList):
                 .format(filepath)
             )
         if not self.options['silent']:
-            print('Records fetched, saving to file...')
+            print('Records fetched, saving to file...', end='\r')
         # child classes much actually implement saving of data
         return filepath
 
@@ -79,6 +253,14 @@ class JSONRecords(BaseRecords):
         self.record_object = JSONRecord
         super().__init__(record_ids, options=options)
 
+    def get(self):
+        """Gets all records in large dict form, converts to list for saving"""
+        master_data_records = super().get()
+        coverted_records = []
+        for master_data_record_id in master_data_records:
+            coverted_records.append(master_data_records[master_data_record_id])
+        return coverted_records
+
     def save(self, filepath, overwrite=False):
         """Gets all data then saves to specified filepath."""
         filepath = super().save(filepath, overwrite)
@@ -87,6 +269,6 @@ class JSONRecords(BaseRecords):
         with open(filepath, mode='w+') as writefile:
             writefile.write(json.dumps(save_data))
         if not self.options['silent']:
-            print('Success!')
+            print('Success!', end='\r')
         # return True if successful
         return True
--- ocre/settings.py
+++ ocre/settings.py
@@ -1,3 +1,4 @@
 ROOT_URL = 'http://numismatics.org/'
 SEARCH_URL = 'ocre/results?q='
 JSON_API_URL = 'ocre/id/'
+NOMISMA_URL = 'http://nomisma.org/id/'
