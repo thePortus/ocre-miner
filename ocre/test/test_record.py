cbdc9a57062ca09005e7c45c9fb5ac0c758b8360
David J. Thomas <dave.a.base@gmail.com>
2018-12-03 14:40:16 -0500

    jsonrecord and jsonrecords testing updated

--- ocre/test/test_record.py
+++ ocre/test/test_record.py
@@ -2,22 +2,16 @@
 
 import unittest
 
-from ..record import JSONRecord
+from ..record import JSONRecord, JSONRecords
 
 
 class TestJSONRecord(unittest.TestCase):
 
     def test_convert_list(self):
         test_data = [
-            {
-                "@id": "http://nomisma.org/id/julia_domna"
-            },
-            {
-                "@id": "http://nomisma.org/id/caracalla"
-            },
-            {
-                "@id": "http://nomisma.org/id/geta"
-            }
+            {"@id": "http://nomisma.org/id/julia_domna"},
+            {"@id": "http://nomisma.org/id/caracalla"},
+            {"@id": "http://nomisma.org/id/geta"}
           ]
         test_results = JSONRecord.convert(test_data, '@id')
         return self.assertTrue(
@@ -26,9 +20,7 @@ class TestJSONRecord(unittest.TestCase):
 
     def test_convert_british_museum(self):
         test_data = [
-            {
-                "@id": "http://nomisma.org/id/julia_domna"
-            },
+            {"@id": "http://nomisma.org/id/julia_domna"},
             {
                 "@id": (
                     "http://collection.britishmuseum.org/id/"
@@ -42,5 +34,21 @@ class TestJSONRecord(unittest.TestCase):
         )
 
     def test_get(self):
-        # records = JSONRecords(['ric.4.ss.159'], options={'silent': True})
-        pass
+        record = JSONRecord('ric.4.ss.159', options={'silent': True})
+        data = record.get()
+        return self.assertTrue(
+            data['obverse_legend'] == 'SEVERVS AVG PART MAX'
+        )
+
+
+class TestJSONRecords(unittest.TestCase):
+
+    def test_get(self):
+        records = JSONRecords(
+            ['ric.4.ss.159', 'ric.4.ss.161A_aureus'], options={'silent': True}
+        )
+        data = records.get()
+        compare_data = 'SEVERVS AVG PART MAX'
+        return self.assertTrue(
+            data[0]['obverse_legend'] == compare_data
+        )
